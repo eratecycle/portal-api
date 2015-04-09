6bbbaf18fc7e707473ea871d39732ba235ffeb7d
Nick Nance <nance.nick@gmail.com>
2015-04-09 07:45:22 -0500

    update DBURI to work with Heroku

--- server/config/env/default.js
+++ server/config/env/default.js
@@ -33,7 +33,7 @@ var defaults = {
   },
   database: {
     // URL to connect to database
-    url: process.env.DBURL || 'mongodb://localhost:27017/erate-cycle',
+    url: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/erate-cycle',
     // Mongoose database options
     options: {
       server: {
