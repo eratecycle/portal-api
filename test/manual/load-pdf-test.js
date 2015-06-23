c245faac66c9425621340a3ebd1b4ed772bfcdaf
Nick Nance <nance.nick@gmail.com>
2015-06-23 09:19:30 -0500

    update manual test to pass arguments

--- test/manual/load-pdf-test.js
+++ test/manual/load-pdf-test.js
@@ -1,6 +1,6 @@
 var parsePDFPath = require('../../server/helpers/verizon-parser');
 
-parsePDFPath('/Users/jakemadden/workspace/eratecycle/portal-api/test/bills/verizon-EA/201404.pdf', function(err, data) {
+parsePDFPath(process.argv[2], function(err, data) {
   if (err) {
     console.log('error:'+err);
     return;
