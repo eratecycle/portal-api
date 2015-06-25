b0642b3a0f6ce73bd2a6b15970f087b1ad7abd4a
Nick Nance <nance.nick@gmail.com>
2015-06-25 09:35:29 -0500

    reorganize functions to make promises easier to follow

--- server/helpers/verizon-parser.js
+++ server/helpers/verizon-parser.js
@@ -16,107 +16,77 @@ global.navigator = { userAgent: "node" };
 global.PDFJS = {};
 global.DOMParser = require('pdfjs-dist/build/pdf.combined').DOMParserMock;
 
-/*
-* Parse a PDF data stream for statement data
-* Safe to use in a browser
-*/
-function parsePDFStatement(data, callback) {
+function processDoc(doc) {
 
-  // reset global variable each time
-  var totalPaymentsFromStatement = 0;
-  totalPaymentsFromTransactions = 0;
-  totalReceiptsFromStatement = 0;
-  totalReceiptsFromTransactions = 0;
-  transactions = [];
-  currentTransactionDate = '';
+  var numPages = doc.numPages;
+  console.info('# Document Loaded');
+  console.info('Number of Pages: ' + numPages);
+  console.info();
 
-  // Will be using promises to load document, pages and misc data instead of
-  // callback.
-  PDFJS.getDocument(data).then(function (doc) {
-
-    var numPages = doc.numPages;
-    console.info('# Document Loaded');
-    console.info('Number of Pages: ' + numPages);
+  var lastPromise; // will be used to chain promises
+  lastPromise = doc.getMetadata().then(function (data) {
+    console.info('# Metadata Is Loaded');
+    console.info('## Info');
+    console.info(JSON.stringify(data.info, null, 2));
     console.info();
-
-    var lastPromise; // will be used to chain promises
-    lastPromise = doc.getMetadata().then(function (data) {
-      console.info('# Metadata Is Loaded');
-      console.info('## Info');
-      console.info(JSON.stringify(data.info, null, 2));
+    if (data.metadata) {
+      console.info('## Metadata');
+      console.info(JSON.stringify(data.metadata.metadata, null, 2));
       console.info();
-      if (data.metadata) {
-        console.info('## Metadata');
-        console.info(JSON.stringify(data.metadata.metadata, null, 2));
-        console.info();
-      }
-    });
-
-    var loadPage = function (pageNum) {
-      return doc.getPage(pageNum).then(function (page) {
-        // console.info('# Page ' + pageNum);
-        var viewport = page.getViewport(1.0 /* scale */);
-        // console.info('Size: ' + viewport.width + 'x' + viewport.height);
-        // console.info();
-        return page.getTextContent().then(function (content) {
-          // Content contains lots of information about the text layout and
-          // styles, but we need only strings at the moment
-          var strings = content.items.map(function (item, i) {
-            //console.info(JSON.stringify(item));
-            // add an appropriate whitespace character here if the next item is on the same line and more than 5px to the right, or is on the next line
-            // item.transform[4] is the x coordinate
-            // item.transform[5] is the y coordinate
-            var nextItem = content.items[i+1];
-            var padding = '';
-            if(nextItem) {
-              var isOnSameLine = nextItem.transform[5] === item.transform[5]; // transform[5] is y coordinate
-              var isFarAway = nextItem.transform[4] - (item.transform[4] + item.width) > 5; // transform[4] is x coordinate
-              //console.info('distance to next item', nextItem.transform[4] - item.transform[4],item.str,nextItem.str);
-              if(!isOnSameLine) {
-                padding = '\\n';
-              }
-              if(isFarAway) {
-                padding = '\t';
-              }
-            }
-            return item.str+padding;
-          });
-          // console.log('## Text Content');
-          var text = strings.join('');
-          // console.log(text);
-          if (pageNum === 1) {
-            var billType = identifyBill(text);
-            // console.log('bill identified as: '+billType);
-          }
-          processStatementPage(text, pageNum);
-          // console.info('# Transactions analysed');
-        }).then(function () {
-          // console.info();
-        });
-      });
-    };
-    // Loading of the first page will wait on metadata and subsequent loadings
-    // will wait on the previous pages.
-    for (var i = 1; i <= numPages; i++) {
-      lastPromise = lastPromise.then(loadPage.bind(null, i));
     }
-    return lastPromise;
-  }).then(function () {
-    console.info('# End of Document');
-    var transactionsList = transactions.map(function(transaction) { return transaction.date+'\t'+transaction.description+'\t'+transaction.amount; }).join('\n');
-    console.info(transactionsList);
-    // console.log('Totals from statement: payments '+totalPaymentsFromStatement+', receipts '+totalReceiptsFromStatement);
-    // console.log('Totals from transactions: payments '+totalPaymentsFromTransactions.toFixed(2)+', receipts '+totalReceiptsFromTransactions.toFixed(2));
-    var errorsInPayments = (totalPaymentsFromTransactions-totalPaymentsFromStatement).toFixed(2),
-    errorsInReceipts = (totalReceiptsFromTransactions-totalReceiptsFromStatement).toFixed(2);
-    console.warn('Errors: payments '+errorsInPayments+', receipts '+errorsInReceipts);
-    callback(null, transactions);
-  }, function (err) {
-    console.error('Error: ' + err);
-    callback(err);
   });
 
-} // end of processPDFStatment()
+  function loadPage(pageNum) {
+    return doc.getPage(pageNum).then(function (page) {
+      // console.info('# Page ' + pageNum);
+      var viewport = page.getViewport(1.0 /* scale */);
+      // console.info('Size: ' + viewport.width + 'x' + viewport.height);
+      // console.info();
+      return page.getTextContent().then(function (content) {
+        // Content contains lots of information about the text layout and
+        // styles, but we need only strings at the moment
+        var strings = content.items.map(function (item, i) {
+          //console.info(JSON.stringify(item));
+          // add an appropriate whitespace character here if the next item is on the same line and more than 5px to the right, or is on the next line
+          // item.transform[4] is the x coordinate
+          // item.transform[5] is the y coordinate
+          var nextItem = content.items[i+1];
+          var padding = '';
+          if(nextItem) {
+            var isOnSameLine = nextItem.transform[5] === item.transform[5]; // transform[5] is y coordinate
+            var isFarAway = nextItem.transform[4] - (item.transform[4] + item.width) > 5; // transform[4] is x coordinate
+            //console.info('distance to next item', nextItem.transform[4] - item.transform[4],item.str,nextItem.str);
+            if(!isOnSameLine) {
+              padding = '\\n';
+            }
+            if(isFarAway) {
+              padding = '\t';
+            }
+          }
+          return item.str+padding;
+        });
+        // console.log('## Text Content');
+        var text = strings.join('');
+        // console.log(text);
+        if (pageNum === 1) {
+          var billType = identifyBill(text);
+          // console.log('bill identified as: '+billType);
+        }
+        processStatementPage(text, pageNum);
+        // console.info('# Transactions analysed');
+      }).then(function () {
+        // console.info();
+      });
+    });
+  };
+  // Loading of the first page will wait on metadata and subsequent loadings
+  // will wait on the previous pages.
+  for (var i = 1; i <= numPages; i++) {
+    lastPromise = lastPromise.then(loadPage.bind(null, i));
+  }
+  return lastPromise;
+}
+
 
 // process the first page of the bill to identify the billType
 function identifyBill(text) {
@@ -222,6 +192,44 @@ function processStatementPage(text, pageNum) {
     });
   }
 }
+
+function endDoc() {
+  console.info('# End of Document');
+  var transactionsList = transactions.map(function(transaction) { return transaction.date+'\t'+transaction.description+'\t'+transaction.amount; }).join('\n');
+  console.info(transactionsList);
+  // console.log('Totals from statement: payments '+totalPaymentsFromStatement+', receipts '+totalReceiptsFromStatement);
+  // console.log('Totals from transactions: payments '+totalPaymentsFromTransactions.toFixed(2)+', receipts '+totalReceiptsFromTransactions.toFixed(2));
+  var errorsInPayments = (totalPaymentsFromTransactions-totalPaymentsFromStatement).toFixed(2),
+  errorsInReceipts = (totalReceiptsFromTransactions-totalReceiptsFromStatement).toFixed(2);
+  console.warn('Errors: payments '+errorsInPayments+', receipts '+errorsInReceipts);
+  callback(null, transactions);
+}
+
+function handleError(err) {
+  console.error('Error: ' + err);
+  callback(err);
+}
+
+/*
+* Parse a PDF data stream for statement data
+* Safe to use in a browser
+*/
+function parsePDFStatement(data, callback) {
+
+  // reset global variable each time
+  var totalPaymentsFromStatement = 0;
+  totalPaymentsFromTransactions = 0;
+  totalReceiptsFromStatement = 0;
+  totalReceiptsFromTransactions = 0;
+  transactions = [];
+  currentTransactionDate = '';
+
+  // Will be using promises to load document, pages and misc data instead of
+  // callback.
+  PDFJS.getDocument(data).then(processDoc).then(endDoc, handleError);
+
+} // end of processPDFStatment()
+
 
 /*
 * Parse a PDF statement given its file path
